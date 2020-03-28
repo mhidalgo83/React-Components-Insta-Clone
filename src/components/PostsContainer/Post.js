cd036d649c71355f949a27ba3cfc6743064c8f3f
Matt <makeshifthidalgo@hotmail.com>
2020-03-27 19:22:36 -0700

    functionality complete for project

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -8,15 +8,11 @@ import "./Posts.css";
 
 // pass props in this file to
 const Post = props => {
-  // set up state for the likes
-
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
@@ -25,7 +21,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likes={props.post.likes} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
@@ -35,5 +31,3 @@ const Post = props => {
 };
 
 export default Post;
-
-
